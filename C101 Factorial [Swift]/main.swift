9735ab109391f9664fe459c91fd54f86527c1b98
Stefano Giron <stefano@giron.org>
2022-11-17 12:12:43 -0600

    kevin sun

---
+++ C101 Factorial [Swift]/main.swift
@@ -0,0 +1,23 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func factorial(from n: Int) -> Int {
+    print("Calculating factorial(\(n))...")
+    if n <= 1 {
+        print("Terminal case, returning 1.")
+        return 1
+    } else {
+        let hello = n * factorial(from: n-1)
+        print("Non-terminal case, returning \(hello).")
+        return hello
+    }
+}
+print("The final result is: \(factorial(from: n)).")
---
+++ C102 Fibonacci [Swift]/.merlin/build/main.swift
@@ -0,0 +1,24 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = -2
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func fibonacci(n: Int) -> Int {
+    print("Calculating fibonacci(\(n))...")
+    if n <= 2 {
+        print("Terminal case, returning 1.")
+        return 1
+    } else {
+        let goodbye = fibonacci(n: n-1) + fibonacci(n: n-2)
+        print("Non-terminal case, returning \(goodbye).")
+        return  goodbye
+    }
+}
+
+print("The final result is: \(fibonacci(n: n)).")
